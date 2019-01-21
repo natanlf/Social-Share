85284030569568d172d0f2b7775310dfcee8db26
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-21 21:36:38 -0200

    Projeto criado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private socialSharing: SocialSharing) {
 
+  }
+
+  share(){
+    this.socialSharing.shareViaWhatsApp("Ola", null, null)
   }
 
 }
